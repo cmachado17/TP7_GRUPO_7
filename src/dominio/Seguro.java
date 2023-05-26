7c72699df1a3df283cd00ead780997702cf9505c
SanUtn <105830363+SanUtn@users.noreply.github.com>
2023-05-26 02:33:04 -0300

    clases Seguro + TipoSeguro

--- src/dominio/Seguro.java
+++ src/dominio/Seguro.java
@@ -1,5 +1,54 @@
 package dominio;
 
 public class Seguro {
+	
+	private int idSeguro;
+	private String descripcion;
+	private int idTipo;
+	private float costoContratacion;
+	private float costoAsegurado;
+	
+	
+	
+	public Seguro() {
+		
+	}
+	
+	
+	public int getIdSeguro() {
+		return idSeguro;
+	}
+	public void setIdSeguro(int idSeguro) {
+		this.idSeguro = idSeguro;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public int getIdTipo() {
+		return idTipo;
+	}
+	public void setIdTipo(int idTipo) {
+		this.idTipo = idTipo;
+	}
 
+	public float getCostoContratacion() {
+		return costoContratacion;
+	}
+	public void setCostoContratacion(float costoContratacion) {
+		this.costoContratacion = costoContratacion;
+	}
+	public float getCostoAsegurado() {
+		return costoAsegurado;
+	}
+	public void setCostoAsegurado(float costoAsegurado) {
+		this.costoAsegurado = costoAsegurado;
+	}
+	@Override
+	public String toString() {
+		return "Seguro [idSeguro=" + idSeguro + ", descripcion=" + descripcion + ", idTipo=" + idTipo
+				+ ", costoContratacion=" + costoContratacion + ", costoAsegurado=" + costoAsegurado + "]";
+	}
 }
---
+++ src/dominio/TipoSeguro.java
@@ -0,0 +1,32 @@
+package dominio;
+
+public class TipoSeguro {
+	
+	private int idTipo;
+	private String descripcion;
+	
+	public TipoSeguro() {
+		
+	}
+
+	public int getIdTipo() {
+		return idTipo;
+	}
+
+	public void setIdTipo(int idTipo) {
+		this.idTipo = idTipo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	@Override
+	public String toString() {
+		return "TipoSeguro [idTipo=" + idTipo + ", descripcion=" + descripcion + "]";
+	}
+}
